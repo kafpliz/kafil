c413a5b80ecf5bb77812813403b7349e68f56f3a
kafpliz <91686116+kafpliz@users.noreply.github.com>
2021-11-03 18:38:58 +0300

    2.11.2021
    dz

---
+++ Source.cpp
@@ -0,0 +1,51 @@
+//Лукашевич артём П43021//
+
+#include <iostream>
+using namespace std;
+/*
+задача 1
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int a,  prozv;
+	cin >> a;
+	int i; 
+	i = 0;
+	while (a < 21 && a > 9 )
+	{
+		i++;
+		cout << a*a << endl;
+
+		break;
+	}
+	return 0;
+}
+задача 2
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int a;
+	cin >> a;
+	while (a > 34 && a < 88)
+	{
+		if (a % 7 == 1 || a % 7 == 2 || a % 7 == 5) cout << a << " делиться на 1 или 2 или 5" << endl;
+		else cout << "neverno"<<endl;
+		break;
+	}
+
+
+
+	return 0;
+
+int main() {
+	int n;
+	cin >> n;
+	int i=0;
+	int sum;
+	while (n>99 && n<1000)
+	{
+		if (sum = n)
+			i++;
+		break;
+	}
+	cout << i << endl;
+	return 0;
+}*/
